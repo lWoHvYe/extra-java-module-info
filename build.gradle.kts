ae49a4b32c7396bc11da2edf01a9cad7ff77724b
王红岩 lWoHvYe <lwohvye@outlook.com>
2023-06-11 06:57:45 +0800

    draft try Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ group = "org.gradlex"
 version = "1.4.1"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
